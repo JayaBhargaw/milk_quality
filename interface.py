87bf763bb419fa30521c4a201a8e73725e17a89c
JayaBhargaw <jayabhargaw02@gmail.com>
2023-02-08 17:29:01 +0530

    modified file

--- interface.py
+++ interface.py
@@ -10,47 +10,63 @@ def home():
 
 @app.route('/predict_milk_quality', methods = ['GET','POST'])
 def predict_milk_quality():
-    #try:
-    if request.method == 'POST':
-        data = request.form.get
+    try:
+        if request.method == 'POST':
+            data = request.form.get
 
-        print("User Data is :",data)
-        pH = int(data('pH'))
-        Temprature = int(data('Temprature'))
-        Taste = data('Taste')
-        Odor = data('Odor')
-        Fat = data('Fat')
-        Turbidity = data('Turbidity')
-        Colour = int(data('Colour'))
+            print("User Data is :",data)
+            pH = eval(data('pH'))
+            Temprature = eval(data('Temprature'))
+            Taste = data('Taste')
+            Odor = data('Odor')
+            Fat = data('Fat')
+            Turbidity = data('Turbidity')
+            Colour = int(data('Colour'))
 
+            if Temprature>90 or Temprature<34:
+                return  jsonify({"Message" : "enter correct value of Temprature"})
 
-        milk_predict = MilkQuality(pH, Temprature, Taste, Odor, Fat , Turbidity,Colour)
-        quality = milk_predict.get_milk_quality()
+            else:
 
-        # return  jsonify({"Result" : f"Medical Insurence Charges will be : {charges}"})
-        return  render_template('index.html',prediction = quality)
+                milk_predict = MilkQuality(pH, Temprature, Taste, Odor, Fat , Turbidity,Colour)
+                quality = milk_predict.get_milk_quality()
 
-    else:
-        data = request.args.get
-        print("User Data is :",data)
+                #return  jsonify({"Result" : f"quality of milk is: {quality}"})
+                return  render_template('index.html',prediction = quality)
 
-        pH = eval(data('pH'))
-        Temprature = eval(data('Temprature'))
-        Taste = data('Taste')
-        Odor = data('Odor')
-        Fat = data('Fat')
-        Turbidity = data('Turbidity')
-        Colour = int(data('Colour'))
+        else:
+            data = request.args.get
+            print("User Data is :",data)
 
-        milk_predict = MilkQuality(pH, Temprature, Taste, Odor, Fat , Turbidity,Colour)
-        quality = milk_predict.get_milk_quality()
+            pH = eval(data('pH'))
+            Temprature = eval(data('Temprature'))
+            Taste = data('Taste')
+            Odor = data('Odor')
+            Fat = data('Fat')
+            Turbidity = data('Turbidity')
+            Colour = int(data('Colour'))
 
-        # return  jsonify({"Result" : f"Medical Insurence Charges will be : {charges}"})
-        return  render_template('index.html',prediction = quality)
+            if Temprature>90 or Temprature<34:
+                return  jsonify({"Message" : "enter correct value of Temprature"})
+
+            else:
+                milk_predict = MilkQuality(pH, Temprature, Taste, Odor, Fat , Turbidity,Colour)
+                quality = milk_predict.get_milk_quality()
+
+            
+            
+                #return  jsonify({"Result" : f"quality of milk is: {quality}"})
+                return  render_template('index.html',prediction = quality)
+
+            
+
+            
+
+            
         
-    # except:
-    #     print(traceback.print_exc())
-    #     return  jsonify({"Message" : "Unsuccessful"})
+    except:
+        print(traceback.print_exc())
+        return  jsonify({"Message" : "Unsuccessful"})
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = config3.PORT_NUMBER,debug=True)
